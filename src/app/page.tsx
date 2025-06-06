793fff4a936a7cef431a5114109e854f23174c81
G-KIBICHII <kibichiigilbert54@gmail.com>
2025-06-06 12:25:41 +0300

    message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
  return(
-  <div className="text-4xl font-bold text-green-500">
-    Hello, world!
-  </div>
+  <Button variant="destructive">
+    click me
+  </Button>
  )
